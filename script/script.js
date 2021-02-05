ef41251cf1b6a51923d4882ba2ec1a13e2965672
Oleksandr Sotnikov <orelovichjob@gmail.com>
2021-02-06 00:24:07 +0200

    fix2 lesson02

--- script/script.js
+++ script/script.js
@@ -2,14 +2,14 @@ alert("Привет");
 console.log('Hello!');
 
 let money = 1000;
-let income = 500;
+let income = '500';
 let addExpenses = '150, 150, 100, 100, 50';
 let deposit = 500;
 let mission = 6000;
 let period = 12;
 let budgetDay = money / 30;
 
-console.log(money, income, deposit);
+console.log(typeof money, typeof income, typeof deposit);
 console.log(addExpenses.length);
 console.log(period = mission);
 console.log(addExpenses.toLowerCase().split(', '));
