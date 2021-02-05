4b5eaa74bf3cc6973d3a45f5e1f13af927c142f3
Oleksandr Sotnikov <orelovichjob@gmail.com>
2021-02-05 21:24:10 +0200

    lesson02

--- script/script.js
+++ script/script.js
@@ -1,8 +1,17 @@
 alert("Привет");
-console.log('Hello!')
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+console.log('Hello!');
+
+let money = 1000;
+let income = 500;
+let addExpenses = '150, 150, 100, 100, 50';
+let deposit = 500;
+let mission = 6000;
+let period = 12;
+
+console.log(money, income, deposit);
+console.log(addExpenses.length);
+console.log(period = mission);
+console.log(addExpenses.toLowerCase(), addExpenses.split(', '));
+
+let budgetDay = money / 30;
+console.log(budgetDay);
